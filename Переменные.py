fb07df3e184b56fad090e9ad62107041caa23638
69odet <odet.o@yandex.ru>
2024-05-20 22:05:53 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,8 @@
+number_of_dz = 12
+print (number_of_dz)
+number_of_hours = 1.5
+print (number_of_hours)
+course_name = 'Python'
+print (course_name)
+print (number_of_hours/number_of_dz)
+print (course_name, 'всего задач:', number_of_dz,'затрачено часов:', number_of_hours, 'среднее время выполнения:', number_of_hours/number_of_dz)
